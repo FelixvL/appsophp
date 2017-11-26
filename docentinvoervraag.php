7ca38f3e047c54b1f28223513f7fea3dc77be110
FelixvL <digitaleservice@gmail.com>
2017-11-26 10:32:20 +0100

    toevoegen vraag werkt

--- docentinvoervraag.php
+++ docentinvoervraag.php
@@ -1,11 +1,29 @@
 <?php
     include "_appsofunctions.php";
     echo showHeader();
+    if(isset($_REQUEST['vraag'])){
+        voerVraagIn($_REQUEST['vraag'], $_REQUEST['code'], $_REQUEST['uitleg']);
+    }
 ?>
 <br>
 <a href=docentbeheer.php>terug</a>
 <br>
 docent invoer vraag
+<br>
+<form action="docentinvoervraag.php" action="post">
+Vraag
+<br>
+<textarea name="vraag"></textarea>
+<br>
+Code
+<br>
+<textarea name="code"></textarea>
+<br>
+Uitleg
+<br>
+<textarea name="uitleg"></textarea>
+<input type="submit" value="voegtoe">
+</form>
 <?php
     echo showFooter();
 ?>
