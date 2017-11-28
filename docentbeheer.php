0609efb6dc57a1a0f6e997172b01c50c38eb28db
FelixvL <digitaleservice@gmail.com>
2017-11-28 14:53:15 +0100

    docent gestyled, weergeven labels studenten werkt, css toegevoegd, editten vraag werkt

--- docentbeheer.php
+++ docentbeheer.php
@@ -2,15 +2,11 @@
     include "_appsofunctions.php";
     echo showHeader();
 ?>
-<br>
-<a href=docentoverzichtstudent.php>bekijk studenten</a>
-<br>
-<a href=docentoverzichtantwoord.php>bekijk antwoorden</a>
-<br>
-<a href=docentinvoervraag.php>nieuwe vraag</a>
-<br>
-Docent beheer
+<a href=docentoverzichtstudent.php class="mainvlak">bekijk studenten</a>
+<a href=docentoverzichtantwoord.php class="mainvlak">bekijk antwoorden</a>
+<a href=docentinvoervraag.php class="mainvlak">nieuwe vraag</a>
+<br><br>
 <?php
-    toonAlleVragen();
+    toonAlleVragenBeheer();
     echo showFooter();
 ?>
