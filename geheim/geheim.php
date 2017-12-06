0eefd5ceaef4ebdfa586bf83781ce3119b4ce08a
FelixvL <digitaleservice@gmail.com>
2017-12-06 20:54:13 +0100

    update

--- geheim/geheim.php
+++
@@ -1,6 +0,0 @@
-<?php
-function connectToDB(){
-    $conn = new mysqli("localhost","root","","appsophp");
-//    $conn = new mysqli("localhost","phpzwollegen1","itphtoren","phpzwollegen2");
-    return $conn;
-}
